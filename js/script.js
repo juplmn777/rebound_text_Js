a4c1c2b8aa0a2ed0eb5388e26e6fe38a0e03ca26
ju <plumyoen.julien@gmail.com>
2020-02-14 15:34:46 +0100

    déplacement horizontal ok

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 const title = document.querySelector("h1");
 const container = document.querySelector("div");
 
-const maxHeight = container.clientHeight;//taille entre les bordures intérieures verticales
-const maxWidth = container.clientWidth;//taille entre les bordures intérieures horizontales
+const maxHeight = container.clientHeight; //taille entre les bordures intérieures verticales
+const maxWidth = container.clientWidth; //taille entre les bordures intérieures horizontales
 
-title.style.position = "relative";//position du texte par rapport a sa div parent
+title.style.position = "relative"; //position du texte par rapport a sa div parent
 
 let topPosition = 0;
 let leftPosition = 0;
-let pushToBottom = -2;//vitesse de déplacement vers le bas
-let pushToRight = -2;//vitesse de déplacement vers la droite
+let pushToBottom = -2; //vitesse de déplacement vers le bas
+let pushToRight = -2; //vitesse de déplacement vers la droite
 
-function animate(){
+function animate() {
     topPosition === 0 ? pushToBottom *= -1 : null;
     topPosition === (maxHeight - title.offsetHeight) ? pushToBottom *= -1 : null;
 
+    leftPosition === 0 ? pushToRight *= -1 : null;
+    leftPosition === (maxWidth - title.offsetWidth) ? pushToRight *= -1 : null;
+
     topPosition += pushToBottom;
+    leftPosition += pushToRight;
 
     title.style.top = topPosition + "px";
+    title.style.left = leftPosition + "px";
 
     requestAnimationFrame(animate);
 }
