50e2374ad8fe9fb6094d07228696b74c3c9e8263
ju <plumyoen.julien@gmail.com>
2020-02-14 15:11:58 +0100

    vitesses de déplacement définies

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ const maxHeight = container.clientHeight;//taille entre les bordures intérieure
 const maxWidth = container.clientWidth;//taille entre les bordures intérieures horizontales
 
 title.style.position = "relative";//position du texte par rapport a sa div parent
+
+let topPosition = 0;
+let leftPosition = 0;
+let pushToBottom = -2;//vitesse de déplacement vers le bas
+let pushToRight = -2;//vitesse de déplacement vers la droite
