aef52d80c37d9573e04ca4cd098af0be8e138c0c
ju <plumyoen.julien@gmail.com>
2020-02-14 15:06:25 +0100

    definition de la position du titre h1 par rapport a son parent

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ const container = document.querySelector("div");
 
 const maxHeight = container.clientHeight;//taille entre les bordures intérieures verticales
 const maxWidth = container.clientWidth;//taille entre les bordures intérieures horizontales
+
+title.style.position = "relative";//position du texte par rapport a sa div parent
