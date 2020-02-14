27973d9a5605e7d3aa36116472d7bf454802b9dc
ju <plumyoen.julien@gmail.com>
2020-02-14 15:04:55 +0100

    récupération des tailles max horizontales et verticales de la div

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 const title = document.querySelector("h1");
 const container = document.querySelector("div");
+
+const maxHeight = container.clientHeight;//taille entre les bordures intérieures verticales
+const maxWidth = container.clientWidth;//taille entre les bordures intérieures horizontales
